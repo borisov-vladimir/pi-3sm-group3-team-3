c150fef183483e737efae54aaed407940d3183a0
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-04-28 18:47:58 +0300

    1

--- kozlova.py
+++ kozlova.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 def var7():
-    st.header('Вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен/нет')
+    st.subheader('Вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен/нет')
 
     df = pd.read_csv('data.csv', delimiter=',')
     df_nullcost = df[(df['Fare'] == 0)]
