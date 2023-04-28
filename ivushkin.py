a4f0b1f7cddc248dd4e9b3167cd5ebbab37a8bf1
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-04-28 19:09:07 +0300

    1

--- ivushkin.py
+++ ivushkin.py
@@ -1,3 +1,9 @@
+import pandas as pd
 import streamlit as st
 def var4():
-    st.text('Здесь будет программа Ивушкина Александра')
+    st.subheader('Вариант 4: Вывести имена пассажиров, стоимость билета которых была выше указанной')
+    data = pd.read_csv('data.csv')
+    fare = st.slider('Выберите минимальное значение цены билета:', min_value=0, max_value=200)
+    passengers = data.loc[data['Fare'] > fare, ['Name', 'Fare']]
+    st.write(passengers)
+var4()
