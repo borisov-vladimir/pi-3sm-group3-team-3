16f5c6313e46dd8a07c89af1332bdaf9781427f7
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-04-28 19:41:00 +0300

    1

--- borisov.py
+++ borisov.py
@@ -10,11 +10,9 @@ def var6():
         sex = 'female'
     x = []
     with open("data.csv") as file:
-        i = 0
         for line in file:
             lst = line.rstrip().split(',')
             if lst[1] == "1" and lst[2] == pclass and lst[5] == sex:
-                i = i + 1
                 name = lst[3] + lst[4]
                 x.append({"Имя": name[1:-1], "Пол": lst[5], "Возраст": lst[6]})
     st.dataframe(x)
--- zayceva.py
+++ zayceva.py
@@ -1,4 +1,25 @@
 import streamlit as st
+
 def var2():
     st.subheader('Вариант 2: Вывести имя, возраст, класс билета пассажиров, выбрав пол и спасен/нет')
-    st.text('Здесь будет программа Зайцевой Наталии')
+    Survived = st.radio('Укажите пассажир спасен:', ['Да', 'Нет'])
+    sex = st.radio("Укажите пол", ['М','Ж'])
+    if sex == 'М':
+        sex = 'male'
+    else:
+        sex = 'female'
+    if Survived == 'Да':
+        Survived = '1'
+    else:
+        Survived = '0'
+
+    result = []
+    with open("data.csv") as file:
+        for line in file:
+            lst = line.rstrip().split(',')
+            if lst[1] == Survived and lst[5] == sex:
+                name = lst[3] + lst[4]
+                result.append({"Имя": name[1:-1], "Возраст": lst[6], "Класс билета": lst[2]})
+    st.dataframe(result)
+
+var2()
