b5f3460912cc539d9533b12f2294bc14ba2945a5
Vladimir_Borisov <mr.vladimir.borisov@mail.ru>
2023-05-14 08:17:40 +0300

    1

--- ivushkin.py
+++ ivushkin.py
@@ -1,8 +1,6 @@
-import pandas as pd
 import streamlit as st
 def var4(data):
     st.subheader('Вариант 4: Вывести имена пассажиров, стоимость билета которых была выше указанной')
-    #data = pd.read_csv('data.csv')
     fare = st.slider('Выберите минимальное значение цены билета:', min_value=0, max_value=200)
     passengers = data.loc[data['Fare'] > fare, ['Name', 'Fare']]
     st.write(passengers)
--- kozlova.py
+++ kozlova.py
@@ -1,11 +1,9 @@
 #Козлова О.С. 3см 7 вариант
 import streamlit as st
-import pandas as pd
 
 def var7(df):
     st.subheader('Вариант 7: вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен/нет')
 
-    #df = pd.read_csv('data.csv', delimiter=',')
     df_nullcost = df[(df['Fare'] == 0)]
 
     choice = st.radio('Вас интересует список:', ['Спасен', 'Не спасен'])
