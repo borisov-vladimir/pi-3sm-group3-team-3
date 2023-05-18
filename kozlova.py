bf491cba5acb5c4edd7ba082d6080529530796fc
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-05-18 20:45:13 +0300

    1

--- kozlova.py
+++ kozlova.py
@@ -7,7 +7,7 @@ def var7_list(df, choice):
     result = df_nullcost[(df_nullcost['Survived'] == choice)]
     return result
 def var7(df):
-    st.subheader('Вариант 7: вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен or нет')
+    st.subheader('Вариант 7: вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен / нет')
     choice = st.radio('Вас интересует список:', ['Спасен', 'Не спасен'])
     if choice == 'Спасен':
         choice = 1
