ec787487d32247c181f0cd5af95efc8a450796a8
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-05-18 20:41:18 +0300

    1

--- kozlova.py
+++ kozlova.py
@@ -3,11 +3,11 @@ import streamlit as st
 import pandas as pd
 
 def var7_list(df, choice):
-    df_nullcost = df[(df['Fare'] == 0)]
+    df_nullcost = df[(df['Fare'] == '0')]
     result = df_nullcost[(df_nullcost['Survived'] == choice)]
     return result
 def var7(df):
-    st.subheader('Вариант 7: вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен или нет')
+    st.subheader('Вариант 7: вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен or нет')
     choice = st.radio('Вас интересует список:', ['Спасен', 'Не спасен'])
     if choice == 'Спасен':
         choice = 1
