02a7c3b8fb1e0332b70d4b71eab2a02d1f8db6d7
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-05-19 15:45:36 +0300

    1

--- ivushkin.py
+++ ivushkin.py
@@ -5,5 +5,5 @@ def var6_list(df, fare):
 
 def var4(df):
     st.subheader('Вариант 4: Вывести имена пассажиров, стоимость билета которых была выше указанной')
-    fare = st.slider('Выберите минимальное значение цены билета:', min_value=0, max_value=500)
+    fare = st.slider('Выберите минимальное значение цены билета:', min_value=0, max_value=270)
     st.write(var6_list(df, fare))
