424b8eeaeddf103fa30d2636bb571c501e75d05b
borisov-vladimir <mr.vladimir.borisov@mail.ru>
2023-05-19 15:44:11 +0300

    1

--- ivushkin.py
+++ ivushkin.py
@@ -1,6 +1,9 @@
 import streamlit as st
-def var4(data):
+def var6_list(df, fare):
+    result = df.loc[df['Fare'] > fare, ['Name', 'Fare']]
+    return result
+
+def var4(df):
     st.subheader('Вариант 4: Вывести имена пассажиров, стоимость билета которых была выше указанной')
-    fare = st.slider('Выберите минимальное значение цены билета:', min_value=0, max_value=200)
-    passengers = data.loc[data['Fare'] > fare, ['Name', 'Fare']]
-    st.write(passengers)
+    fare = st.slider('Выберите минимальное значение цены билета:', min_value=0, max_value=500)
+    st.write(var6_list(df, fare))
